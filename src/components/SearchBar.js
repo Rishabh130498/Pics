0400dca60f7f42b602b8c4b2c15298dd5275cfd4
Rishabh Agarwal <Rishabh130498@gmail.com>
2020-12-19 23:46:47 +0530

    inline/abbreviated code for event handler. Also, event can be replaced by e

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,17 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
-
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={this.onInputChange} />
+            <input
+              type="text"
+              onChange={(e) => console.log(e.target.value)}
+              //event can be replaced by a single word "e" or vice-versa
+            />
           </div>
         </form>
       </div>
