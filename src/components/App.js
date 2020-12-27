cfab970f0528135f1aae14250c563880c4f407b1
Rishabh Agarwal <Rishabh130498@gmail.com>
2020-12-27 19:06:27 +0530

    requesting data from Upsplash API and watch it showing data in network chrome inspect tool

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID 1G5eE0b0gqIqLOj_XGQ-B2o-NwGRj-J9OY4wGm8mg0g",
+      },
+    });
   }
 
   render() {
