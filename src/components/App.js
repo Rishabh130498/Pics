1e405cab31713566fa3cce87abd1cffdbf3326d0
Rishabh Agarwal <Rishabh130498@gmail.com>
2020-12-29 18:25:52 +0530

    coverting onSearchSubmit into arrow function to remove this.setState is not a function error

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,24 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
         Authorization: "Client-ID 1G5eE0b0gqIqLOj_XGQ-B2o-NwGRj-J9OY4wGm8mg0g",
       },
     });
-  }
+
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
